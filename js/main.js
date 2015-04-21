0b7513ee854d37d56c2d77c8d9f5ee5818cb52c6
jlogzz <juan_lorenzo_@hotmail.com>
2015-04-21 15:31:20 -0500

    Init de app

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+$(document).ready(function(){
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  $("#login-btn").click(function(){
+    window.location = "./app/app.html";
+  });
+});
