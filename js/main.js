4b27991584973401c958a1adb16970ba217a0f80
jlogzz <juan_lorenzo_@hotmail.com>
2015-04-23 11:15:06 -0500

    Cerrar programa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
 
 $(document).ready(function(){
+  $("#close").click(function(){
+    if(confirm("SALIR DEL PROGRAMA"))
+      win.close();
+  });
+
   // Initialize collapse button
   $(".button-collapse").sideNav();
   $("#login-btn").click(function(){
